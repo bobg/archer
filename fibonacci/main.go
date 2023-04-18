8136eadc9b76530dbff0e2a4dbd4bd4cd5f67a39
Bob Glickstein <bobg@users.noreply.github.com>
2023-04-18 10:32:46 -0700

    Create main.go

---
+++ fibonacci/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(fibonacci(7))
+}
+
+func fibonacci(n int) int {
+	// Fill me in!
+	// When n is 0 or 1, fibonacci(n) should just be n.
+	// When n is any larger number,
+	// fibonacci(n) should be the sum of the previous two fibonacci numbers.
+}
